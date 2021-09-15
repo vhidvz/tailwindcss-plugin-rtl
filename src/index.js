8371126b33b24463b2270c31fc69de67a7adceec
Vahid V <vhid.vz@gmail.com>
2021-09-15 19:18:55 +0430

    some useful utilities was added.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-module.exports = function ({ addVariant, e }) {
+module.exports = function ({ addVariant, addUtilities, e }) {
   addVariant("rtl", ({ modifySelectors, separator }) => {
     modifySelectors(({ className }) => {
       return `[dir='rtl'] .${e(`rtl${separator}${className}`)}`;
     });
   });
+
+  const newUtilities = {
+    // margin
+    ".mr-unset": {
+      "margin-right": "unset !important",
+    },
+    ".ml-unset": {
+      "margin-left": "unset !important",
+    },
+    // padding
+    ".pr-unset": {
+      "padding-right": "unset !important",
+    },
+    ".pl-unset": {
+      "padding-left": "unset !important",
+    },
+  };
+
+  addUtilities(newUtilities, ["responsive", "rtl"]);
 };
